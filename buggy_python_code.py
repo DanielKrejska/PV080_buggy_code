71e8fbd7ebe840dda284568d137e449efa3c4171
DanielKrejska <dankrejska@seznam.cz>
2024-04-15 11:05:29 +0200

    another fix

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -26,7 +26,7 @@ def print_nametag(format_string, person):
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     version = "2"
-    if (urllib_version == "3")
+    if (urllib_version == "3"):
         version = "3"
     exec(f"import urllib{version} as urllib", globals())
     # Fetch and print the requested URL
